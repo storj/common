faa503e96309478558343420893ca740c8bbd46b
Michal Niewrzal <michal@storj.io>
2020-01-07 17:03:46 +0100

    storj: move CalcNeededNodes to storj.RedundancyScheme
    Move CalcNeededNodes function closer to storj.RedundancyScheme because
    we want to remove `uplink/storage` and we need to distribute code from
    this package.
    
    https://storjlabs.atlassian.net/browse/V3-517
    
    Change-Id: I3b8c6d28658dc3265d1a2db9fa83867c08420c93

--- storj/redundancy.go
+++ storj/redundancy.go
@@ -38,6 +38,27 @@ func (scheme RedundancyScheme) StripeSize() int32 {
 	return scheme.ShareSize * int32(scheme.RequiredShares)
 }
 
+// DownloadNodes calculates how many minimum nodes are needed for download,
+// based on t = k + (n-o)k/o.
+func (scheme RedundancyScheme) DownloadNodes() int32 {
+	extra := int32(1)
+
+	if scheme.OptimalShares > 0 {
+		extra = int32(((scheme.TotalShares - scheme.OptimalShares) * scheme.RequiredShares) / scheme.OptimalShares)
+		if extra == 0 {
+			// ensure there is at least one extra node, so we can have error detection/correction
+			extra = 1
+		}
+	}
+
+	needed := int32(scheme.RequiredShares) + extra
+
+	if needed > int32(scheme.TotalShares) {
+		needed = int32(scheme.TotalShares)
+	}
+	return needed
+}
+
 // RedundancyAlgorithm is the algorithm used for redundancy
 type RedundancyAlgorithm byte
 
