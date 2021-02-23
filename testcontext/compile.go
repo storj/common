4de910503c2c304b7ab28316686d3f9dfeafdee9
Egon Elbre <egonelbre@gmail.com>
2021-02-23 12:23:01 +0200

    testcontext: move CompileC and CompileShared to uplink-c
    Change-Id: Ic96f4e0c456e2bba51406e5dfcaea34729f1a0e0

--- testcontext/compile.go
+++ testcontext/compile.go
@@ -8,20 +8,7 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
-	"runtime"
-	"testing"
 )
-
-// CLibMath is the standard C math library (see `man math.h`).
-var CLibMath = Include{Standard: true, Library: "m"}
-
-// CompileCOptions stores options for compiling C source to an executable.
-type CompileCOptions struct {
-	Dest     string
-	Sources  []string
-	Includes []Include
-	NoWarn   bool
-}
 
 // Compile compiles the specified package and returns the executable name.
 func (ctx *Context) Compile(pkg string, preArgs ...string) string {
@@ -109,89 +96,4 @@ func (ctx *Context) CompileWithLDFlagsX(pkg string, ldFlagsX map[string]string) 
 	}
 
 	return ctx.Compile(pkg, "-ldflags", ldFlags)
-}
-
-// CompileShared compiles pkg as c-shared.
-//
-// Note: cgo header paths are currently relative to package root.
-// TODO: support inclusion from other directories.
-func (ctx *Context) CompileShared(t *testing.T, name string, pkg string) Include {
-	t.Helper()
-
-	base := ctx.File("build", name)
-
-	args := []string{"build", "-buildmode", "c-shared"}
-	args = append(args, "-o", base+".so", pkg)
-
-	// not using race detector for c-shared
-	cmd := exec.Command("go", args...)
-	t.Log("exec:", cmd.Args)
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Error(string(out))
-		t.Fatal(err)
-	}
-	t.Log(string(out))
-
-	return Include{Header: base + ".h", Library: base + ".so"}
-}
-
-// CompileC compiles file as with gcc and adds the includes.
-func (ctx *Context) CompileC(t *testing.T, opts CompileCOptions) string {
-	t.Helper()
-
-	exe := ctx.File("build", opts.Dest+".exe")
-
-	var args = []string{}
-	if !opts.NoWarn {
-		args = append(args, "-Wall")
-		args = append(args, "-Wextra")
-		args = append(args, "-Wpedantic")
-		args = append(args, "-Werror")
-	}
-	args = append(args, "-ggdb")
-	args = append(args, "-o", exe)
-	for _, inc := range opts.Includes {
-		if inc.Header != "" {
-			args = append(args, "-I", filepath.Dir(inc.Header))
-		}
-		if inc.Library != "" {
-			if inc.Standard {
-				args = append(args,
-					"-l"+inc.Library,
-				)
-				continue
-			}
-			if runtime.GOOS == "windows" {
-				args = append(args,
-					"-L"+filepath.Dir(inc.Library),
-					"-l:"+filepath.Base(inc.Library),
-				)
-			} else {
-				args = append(args, inc.Library)
-			}
-		}
-	}
-	args = append(args, opts.Sources...)
-
-	/* #nosec G204 */ // This package is used for testing and the parameter's value are controlled by the above logic
-	cmd := exec.Command("gcc", args...)
-	t.Log("exec:", cmd.Args)
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Error(string(out))
-		t.Fatal(err)
-	}
-	t.Log(string(out))
-
-	return exe
-}
-
-// Include defines an includable library for gcc.
-type Include struct {
-	Header   string
-	Library  string
-	Standard bool
 }
