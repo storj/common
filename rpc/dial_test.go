0700f883bc0f346a591faa3f3856021345f806eb
Egon Elbre <egonelbre@gmail.com>
2021-07-27 16:08:40 +0300

    rpc: fix dialing tests
    Tests were using hardcoded ports and not waiting for goroutines to
    finish.
    
    Change-Id: Id7fb87cceafb449afa80661f4f469195efa0a865

--- rpc/dial_test.go
+++ rpc/dial_test.go
@@ -13,32 +13,41 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"errors"
+	"io"
 	"math/big"
 	"net"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/zeebo/errs"
 
+	"storj.io/common/sync2"
+	"storj.io/common/testcontext"
 	"storj.io/drpc/drpcmigrate"
 )
 
 func TestDialerUnencrypted(t *testing.T) {
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+
 	d := NewDefaultPooledDialer(nil)
 
-	lis, err := net.Listen("tcp", ":0")
+	lis, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
-	defer func() { _ = lis.Close() }()
+	defer ctx.Check(lis.Close)
 
-	conn, err := d.DialAddressUnencrypted(context.Background(), lis.Addr().String())
+	conn, err := d.DialAddressUnencrypted(ctx, lis.Addr().String())
 	require.NoError(t, err)
 	require.NoError(t, conn.Close())
 }
 
 func TestDialHostnameVerification(t *testing.T) {
-	ctx := context.Background()
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
 
-	certificatePEM, privateKeyPEM := createCertificate(t, "localhost")
+	certificatePEM, privateKeyPEM := createTestingCertificate(t, "localhost")
 
 	certificate, err := tls.X509KeyPair(certificatePEM, privateKeyPEM)
 	require.NoError(t, err)
@@ -48,50 +57,52 @@ func TestDialHostnameVerification(t *testing.T) {
 	}
 
 	// start a server with the certificate
-	tcpListener, err := net.Listen("tcp", ":22111")
+	tcpListener, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
+	serverAddr := tcpListener.Addr().String()
 
 	listenMux := drpcmigrate.NewListenMux(tcpListener, len(drpcmigrate.DRPCHeader))
-	go func() {
-		err := listenMux.Run(ctx)
-		require.NoError(t, err)
-	}()
+
+	listenCtx, listenCancel := context.WithCancel(ctx)
+	defer listenCancel()
+	ctx.Go(func() error {
+		return listenMux.Run(listenCtx)
+	})
 
 	drpcListener := tls.NewListener(listenMux.Route(drpcmigrate.DRPCHeader), serverTLSConfig)
-	defer func(drpcListener net.Listener) {
-		err := drpcListener.Close()
-		require.NoError(t, err)
-	}(drpcListener)
+	defer ctx.Check(drpcListener.Close)
 
-	serverErrorChannel := make(chan error, 20)
-	acceptConnectionSuccess := func() {
-		connection, err := drpcListener.Accept()
+	acceptConnectionSuccess := func() error {
+		conn, err := drpcListener.Accept()
 		if err != nil {
-			serverErrorChannel <- err
-			return
+			return errs.Wrap(err)
 		}
+		defer func() { _ = conn.Close() }()
 
 		buffer := make([]byte, 256)
-		_, err = connection.Read(buffer)
-		if err != nil {
-			serverErrorChannel <- err
-			return
+		_, err = conn.Read(buffer)
+		if errors.Is(err, io.EOF) {
+			err = nil
 		}
+		if err != nil {
+			return errs.Wrap(err)
+		}
+		return nil
 	}
 
-	acceptConnectionFailure := func() {
-		connection, err := drpcListener.Accept()
+	acceptConnectionFailure := func() error {
+		conn, err := drpcListener.Accept()
 		if err != nil {
-			serverErrorChannel <- err
-			return
+			return errs.Wrap(err)
 		}
+		defer func() { _ = conn.Close() }()
 
 		buffer := make([]byte, 256)
-		_, err = connection.Read(buffer)
+		_, err = conn.Read(buffer)
 		if err == nil {
-			serverErrorChannel <- errors.New("expected connection failure, but there is no error")
-			return
+			return errs.New("expected connection failure, but there is no error")
 		}
+		return nil
 	}
 
 	// create client
@@ -99,49 +110,75 @@ func TestDialHostnameVerification(t *testing.T) {
 	certPool.AppendCertsFromPEM(certificatePEM)
 
 	// happy scenario 1 get hostname from address
-	dialer := NewDefaultPooledDialer(nil)
-	dialer.HostnameTLSConfig = &tls.Config{
-		RootCAs: certPool,
-	}
-	go acceptConnectionSuccess()
-	connection, err := dialer.DialAddressHostnameVerification(ctx, "localhost:22111")
-	require.NoError(t, err)
-	require.NotNil(t, connection)
+	require.Empty(t, sync2.Concurrently(
+		acceptConnectionSuccess,
+		func() error {
+			dialer := NewDefaultDialer(nil)
+			dialer.HostnameTLSConfig = &tls.Config{
+				RootCAs: certPool,
+			}
+
+			// use a domain name to ensure we can get hostname from the address
+			localAddr := strings.ReplaceAll(serverAddr, "127.0.0.1", "localhost")
+			conn, err := dialer.DialAddressHostnameVerification(ctx, localAddr)
+			if err != nil {
+				return errs.Wrap(err)
+			}
+			return errs.Wrap(conn.Close())
+		},
+	))
 
 	// happy scenario 2
-	dialer = NewDefaultPooledDialer(nil)
-	dialer.HostnameTLSConfig = &tls.Config{
-		RootCAs:    certPool,
-		ServerName: "localhost",
-	}
-	go acceptConnectionSuccess()
-	// Can't verify IPv6 during ci because of docker default
-	// connection, err = dialer.DialAddressHostnameVerification(ctx, "[::1]:22111", clientTLSConfig)
-	connection, err = dialer.DialAddressHostnameVerification(ctx, "127.0.0.2:22111")
-	require.NoError(t, err)
-	require.NotNil(t, connection)
+	require.Empty(t, sync2.Concurrently(
+		acceptConnectionSuccess,
+		func() error {
+			dialer := NewDefaultDialer(nil)
+			dialer.HostnameTLSConfig = &tls.Config{
+				RootCAs:    certPool,
+				ServerName: "localhost",
+			}
+			// Can't verify IPv6 during ci because of docker default
+			// connection, err = dialer.DialAddressHostnameVerification(ctx, "[::1]:22111", clientTLSConfig)
+			conn, err := dialer.DialAddressHostnameVerification(ctx, serverAddr)
+			if err != nil {
+				return errs.Wrap(err)
+			}
+			return errs.Wrap(conn.Close())
+		},
+	))
 
 	// failure scenario invalid certificate
-	dialer = NewDefaultPooledDialer(nil)
-	dialer.HostnameTLSConfig = &tls.Config{
-		RootCAs:    certPool,
-		ServerName: "example.com",
-	}
-	go acceptConnectionFailure()
-	connection, err = dialer.DialAddressHostnameVerification(ctx, "127.0.0.1:22111")
-	require.Error(t, err)
-	require.EqualError(t, err, "rpc: x509: certificate is valid for localhost, not example.com")
-	require.Nil(t, connection)
+	require.Empty(t, sync2.Concurrently(
+		acceptConnectionFailure,
+		func() error {
+			dialer := NewDefaultDialer(nil)
+			dialer.HostnameTLSConfig = &tls.Config{
+				RootCAs:    certPool,
+				ServerName: "storj.test",
+			}
+			conn, err := dialer.DialAddressHostnameVerification(ctx, serverAddr)
+			if err == nil {
+				_ = conn.Close()
+				return errs.New("expected an error")
+			}
+			if conn != nil {
+				return errs.New("expected conn to be nil")
+			}
+			if err.Error() != "rpc: x509: certificate is valid for localhost, not storj.test" {
+				return errs.New("expected an error, got: %w", err)
+			}
+			return nil
+		},
+	))
 
-	require.Empty(t, serverErrorChannel, "Serverside errors occurred while testing the client")
-
-	dialer = NewDefaultPooledDialer(nil)
-	_, err = dialer.DialAddressHostnameVerification(ctx, "tweakers.net")
+	// test invalid hostname
+	dialer := NewDefaultDialer(nil)
+	_, err = dialer.DialAddressHostnameVerification(ctx, "storj.test")
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "missing port in address")
 }
 
-func createCertificate(t *testing.T, hostname string) (certificatePEM []byte, privateKeyPEM []byte) {
+func createTestingCertificate(t *testing.T, hostname string) (certificatePEM []byte, privateKeyPEM []byte) {
 	notAfter := time.Now().Add(1 * time.Minute)
 
 	// first create a server certificate
