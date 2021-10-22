a5fb55efc9fedbfb2ff3b0c6242c11c2b7e025f6
Mya <j.mya.pitz@gmail.com>
2021-10-22 11:27:04 -0500

    geoip: add new library for interacting with geoip data

---
+++ geoip/doc.go
@@ -0,0 +1,12 @@
+/*
+Package geoip provides utility code used across storj projects for leveraging the Maxmind GeoIP datasets. The maxminddb
+package was not added as a dependency as this utility code is not needed in every project. Projects consuming the
+database should include it in their dependencies. Example usage:
+
+reader, err := maxminddb.Open("path/to/GeoIP2Lite.mmdb")
+if err != nil {
+	return errs.New("unable to open geolocation db: %w", err)
+}
+peer.IPDB = geoip.NewIPDB(reader)
+*/
+package geoip
