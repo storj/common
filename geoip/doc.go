7db3a6aec2f7d3d4f2591d792d415b89c275acaf
Mya <j.mya.pitz@gmail.com>
2021-10-22 11:46:20 -0500

    missed copyright header

--- geoip/doc.go
+++ geoip/doc.go
@@ -1,3 +1,6 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// See LICENSE for copying information.
+
 /*
 Package geoip provides utility code used across storj projects for leveraging the Maxmind GeoIP datasets. The maxminddb
 package was not added as a dependency as this utility code is not needed in every project. Projects consuming the
