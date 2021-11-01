93824b1605ab3cb16587847dadfdf3697bf2aedd
dlamarmorgan <damein@storj.io>
2021-11-01 11:18:37 -0700

    testrand: add functions to generate random numeric and alphanumeric byte slices
    Change-Id: I65b1215ae2ef34442b2583ffd5591901f4f3e64e

--- testrand/rand_test.go
+++ testrand/rand_test.go
@@ -4,13 +4,28 @@
 package testrand_test
 
 import (
+	"regexp"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"storj.io/common/testrand"
 )
+
+var stringLength = 50
 
 func TestNoPanic(t *testing.T) {
 	t.Log("URLPath", testrand.URLPath())
 	t.Log("URLPathNonFolder", testrand.URLPathNonFolder())
 	t.Log("Path", testrand.Path())
+	isNumeric := regexp.MustCompile(`^[0-9]+$`).MatchString
+	randomNumericString := string(testrand.RandNumeric(stringLength))
+	t.Log("Numeric String", randomNumericString)
+	require.True(t, isNumeric(randomNumericString))
+	require.Equal(t, stringLength, len(randomNumericString))
+	isAlphaNumeric := regexp.MustCompile(`^[A-Za-z0-9]+$`).MatchString
+	randomAlphaNumericString := string(testrand.RandAlphaNumeric(stringLength))
+	t.Log("AlphaNumeric String", randomAlphaNumericString)
+	require.True(t, isAlphaNumeric(randomAlphaNumericString))
+	require.Equal(t, stringLength, len(randomAlphaNumericString))
 }
