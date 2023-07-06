392151c364ff393326b27184a2a9fcc378a8dc1c
Egon Elbre <egonelbre@gmail.com>
2023-07-06 16:47:19 +0300

    storj/location: implement Set
    We need to often check whether a country code is in set.
    Implement a bitset, which should speed up these significantly.
    
    Change-Id: I58140f02aa88b25f32516988611bd2f4bb8ef32c

---
+++ storj/location/set.go
@@ -0,0 +1,61 @@
+// Copyright (C) 2023 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package location
+
+import "math/bits"
+
+// Set implements a data-structure for fast lookups for country codes.
+type Set [(len(countryISOCode) + 31) / 32]uint32
+
+// NewSet returns a set that has the specific countries set.
+func NewSet(countries ...CountryCode) (r Set) {
+	for _, c := range countries {
+		r.Include(c)
+	}
+	return r
+}
+
+// NewFullSet returns a set that has every bit filled.
+func NewFullSet() (r Set) {
+	for i := range r {
+		r[i] = 0xFFFFFFFF
+	}
+	return r
+}
+
+// Contains checks whether c exists in the set.
+func (set *Set) Contains(c CountryCode) bool {
+	bucket, bit := c/32, c%32
+	if int(bucket) >= len(*set) {
+		return false
+	}
+	return set[bucket]&(1<<bit) != 0
+}
+
+// Include adds c to the set.
+func (set *Set) Include(c CountryCode) {
+	bucket, bit := c/32, c%32
+	if int(bucket) >= len(*set) {
+		return
+	}
+	set[bucket] |= 1 << bit
+}
+
+// Remove removes c from the set.
+func (set *Set) Remove(c CountryCode) {
+	bucket, bit := c/32, c%32
+	if int(bucket) >= len(*set) {
+		return
+	}
+	set[bucket] &^= 1 << bit
+}
+
+// Count returns the number of items in the set.
+func (set *Set) Count() int {
+	total := 0
+	for _, v := range *set {
+		total += bits.OnesCount32(v)
+	}
+	return total
+}
---
+++ storj/location/set_test.go
@@ -0,0 +1,44 @@
+// Copyright (C) 2023 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package location
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSet(t *testing.T) {
+	var set Set
+	set.Include(Belgium)
+	set.Include(Hungary)
+	set.Include(Netherlands)
+
+	require.True(t, set.Contains(Belgium))
+	require.True(t, set.Contains(Hungary))
+	require.True(t, set.Contains(Netherlands))
+	require.False(t, set.Contains(Estonia))
+	require.False(t, set.Contains(Austria))
+
+	require.Equal(t, 3, set.Count())
+
+	set.Remove(Hungary)
+	// removing non-existent things should be fine
+	set.Remove(Estonia)
+	set.Remove(Austria)
+	require.Equal(t, 2, set.Count())
+}
+
+func TestSet_Full(t *testing.T) {
+	var set Set
+	for c := CountryCode(0); int(c) < len(countryISOCode); c++ {
+		set.Include(c)
+	}
+	require.Equal(t, len(countryISOCode), set.Count())
+
+	for c := CountryCode(0); int(c) < len(countryISOCode); c++ {
+		set.Remove(c)
+	}
+	require.Equal(t, 0, set.Count())
+}
