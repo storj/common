5603ebfc2fe6b943f6a352100ace3e45a59526db
Egon Elbre <egonelbre@gmail.com>
2024-02-29 14:12:40 +0200

    scripts: use check-dependency tool
    Change-Id: I3024894034de8d01cff428ec2a23d5450036e6dc

--- scripts/check-dependencies.sh
+++ scripts/check-dependencies.sh
@@ -1,44 +1,25 @@
 #!/usr/bin/env bash
-set -ueo pipefail
+set -uo pipefail
 set +x
 
 # This script verifies that we don't accidentally import specific packages.
 
-
-
-for package in $(find -name "*.go" | xargs -n1 dirname | sort | uniq | \
-     grep -v test | awk '{print $1}' | \
-     grep -v -E "cockroachutil|dbutil"); do
-    if go list -deps $package |  grep -Eq "github.com/(lib/pq|jackc/pg)"; then
-        echo "postgres dependencies are allowed only in white-listed packages, but not in $package";
-        exit -1;
-    fi
-done
-
-if go list -deps -test ./... | grep -q "redis"; then
-    echo "common must not have a dependency to redis";
+if ! check-dependency -ignore "cockroachutil;dbutil" -check "github.com/(lib/pq|jackc/pg)" ./...; then
+    echo "we shouldn't import postgres outside of database packages";
     exit -1;
 fi
 
-if go list -deps -test ./... | grep -q "bolt"; then
-    echo "common must not have a dependency to bolt";
+if ! check-dependency -check "redis;bolt" ./...; then
+    echo "common must not have a dependency to redis or bolt";
     exit -1;
 fi
 
-for package in $(find -name "*.go" | xargs -n1 dirname | sort | uniq | \
-     grep -v test | awk '{print $1}' | \
-     grep -v -E "process|scripts"); do
-    if go list -deps $package | grep -q "^test$"; then
-        echo "test dependency is allowed only in white-listed packages, but not in $package";
-        exit -1;
-    fi
-done
+if ! check-dependency -ignore "test;common/process" -check "test" -except "internal/testlog" ./...; then
+    echo "packages not related to test functionality should not bring in testing related things";
+    exit -1;
+fi
 
-for package in $(find -name "*.go" | xargs -n1 dirname | sort | uniq | \
-     grep -v test | awk '{print $1}' | \
-     grep -v -E "httpranger|requestid|test|gen|cfgstruct|dbutil|debug|identity|metrics|migrate|process|tagsql|traces|version"); do
-    if go list -deps $package | grep -q "net/http"; then
-        echo "net/http dependency is allowed only in white-listed packages, but not in $package";
-        exit -1;
-    fi
-done
+if ! check-dependency -ignore "cfgstruct;dbutil;debug;httpranger;metrics;migrate;process;requestid;storj/location/gen;tagsql;test;traces;version" -check "net/http" ./...; then
+    echo "net/http is a huge dependency that we don't want to accidentally introduce into uplink";
+    exit -1;
+fi
