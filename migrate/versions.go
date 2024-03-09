cb9e95e83f744e12a08c75c0467e2ccd1dd1158a
Egon Elbre <egonelbre@gmail.com>
2024-03-09 21:48:40 +0200

    migrate: make error message even better
    Change-Id: Ic2b668d454d0db925572ea41dad279b78e2e6f1a

--- migrate/versions.go
+++ migrate/versions.go
@@ -135,6 +135,9 @@ func (migration *Migration) ValidateVersions(ctx context.Context, log *zap.Logge
 		}
 
 		if expectedVersion != currentVersion {
+			if currentVersion < 0 {
+				return ErrValidateVersionMismatch.New("expected %d, but database is uninitialized (version %d)", expectedVersion, currentVersion)
+			}
 			return ErrValidateVersionMismatch.New("expected %d, but current version is %d", expectedVersion, currentVersion)
 		}
 	}
