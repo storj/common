b4e15243fdb010d21389763873d818c1b9e7cf5e
Jeremy Wharton <jeremy.e.wharton@gmail.com>
2024-03-15 01:56:01 -0500

    storj: add retention mode constants
    This change introduces constants for Object Lock retention modes.
    
    Change-Id: Iba59177e3fbcadddc5778a0dc99eaa64f1490cab

---
+++ storj/retention.go
@@ -0,0 +1,16 @@
+// Copyright (C) 2024 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package storj
+
+// RetentionMode represents the retention mode of an object version.
+type RetentionMode uint8
+
+const (
+	// NoRetention signifies that a retention period has not been set on an object version.
+	NoRetention RetentionMode = 0
+
+	// ComplianceMode signifies that an object version is locked in compliance mode
+	// and cannot be deleted or modified until the retention period expires.
+	ComplianceMode RetentionMode = 1
+)
