679f2e38b6f49a39919bc55654da98b97ed0e9e4
Jeff Wendling <leterip@gmail.com>
2024-07-09 14:15:59 -0400

    debug: add remote debug trace rpc endpoint
    Change-Id: I8198314d57dacd93b8a267c6e67403dae542709d

---
+++ debug/endpoint.go
@@ -0,0 +1,56 @@
+// Copyright (C) 2024 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package debug
+
+import (
+	"context"
+	"runtime/trace"
+
+	"github.com/zeebo/errs"
+
+	"storj.io/common/pb"
+	"storj.io/common/rpc/rpcstatus"
+)
+
+// Endpoint implements a remote debug server.
+type Endpoint struct {
+	pb.DRPCDebugUnimplementedServer
+
+	Auth func(ctx context.Context) error
+}
+
+// NewEndpoint constructs a RemoteDebugEndpoint that will consult the given auth function
+// with the request context to determine if the request is authorized.
+func NewEndpoint(auth func(ctx context.Context) error) *Endpoint {
+	return &Endpoint{Auth: auth}
+}
+
+// CollectRuntimeTraces will stream trace data to the client until the client cancels the request
+// either explicitly or some error happens in sending.
+func (f *Endpoint) CollectRuntimeTraces(_ *pb.CollectRuntimeTracesRequest, stream pb.DRPCDebug_CollectRuntimeTracesStream) error {
+	if err := f.Auth(stream.Context()); err != nil {
+		return rpcstatus.Wrap(rpcstatus.Unauthenticated, err)
+	}
+
+	if err := trace.Start(&streamWriter{stream: stream}); err != nil {
+		return rpcstatus.Wrap(rpcstatus.FailedPrecondition, errs.New("trace failed to start: %w", err))
+	}
+	defer trace.Stop()
+
+	<-stream.Context().Done()
+
+	return nil
+}
+
+type streamWriter struct {
+	stream pb.DRPCDebug_CollectRuntimeTracesStream
+}
+
+func (s *streamWriter) Write(p []byte) (int, error) {
+	if err := s.stream.Send(&pb.CollectRuntimeTracesResponse{Data: p}); err != nil {
+		_ = s.stream.Close()
+		return 0, err
+	}
+	return len(p), nil
+}
---
+++ debug/endpoint_test.go
@@ -0,0 +1,72 @@
+// Copyright (C) 2024 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package debug
+
+import (
+	"context"
+	"errors"
+	"runtime/trace"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+
+	"storj.io/common/pb"
+	"storj.io/common/sync2"
+	"storj.io/drpc"
+)
+
+func TestRemoteDebugServer_Unauthenticated(t *testing.T) {
+	endpoint := NewEndpoint(func(ctx context.Context) error {
+		return errors.New("unauthenticated")
+	})
+
+	stream := newFakeStream(context.Background())
+	require.Error(t, endpoint.CollectRuntimeTraces(nil, stream), "unauthenticated")
+}
+
+func TestRemoteDebugServer_SomeData(t *testing.T) {
+	if trace.IsEnabled() {
+		t.Skip("tracing already enabled")
+	}
+
+	endpoint := NewEndpoint(func(ctx context.Context) error { return nil })
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	stream := newFakeStream(ctx)
+
+	var group errgroup.Group
+	group.Go(func() error {
+		return endpoint.CollectRuntimeTraces(nil, stream)
+	})
+	group.Go(func() error {
+		<-stream.written.Done()
+		cancel()
+		return nil
+	})
+	require.NoError(t, group.Wait())
+
+	require.NotZero(t, len(stream.data))
+}
+
+type fakeStream struct {
+	drpc.Stream // expected nil but just implements interface
+
+	ctx     context.Context
+	written sync2.Fence
+	data    []byte
+}
+
+func newFakeStream(ctx context.Context) *fakeStream { return &fakeStream{ctx: ctx} }
+
+func (f *fakeStream) Context() context.Context { return f.ctx }
+func (f *fakeStream) Send(m *pb.CollectRuntimeTracesResponse) error {
+	f.data = append(f.data, m.Data...)
+	if len(f.data) > 0 {
+		f.written.Release()
+	}
+	return nil
+}
