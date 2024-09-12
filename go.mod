c0c47dc29560ee47c7d1cee7061613d5670e7559
Jeremy Wharton <jeremy.e.wharton@gmail.com>
2024-09-12 00:21:36 -0500

    pb: make object legal hold status nullable
    The object protobuf definition contains a field indicating an object's
    legal hold status. This field was typed as a regular boolean because
    it was thought that we would only need to indicate whether legal hold
    was enabled or disabled. However, in order to be consistent with Amazon
    S3, we must be able to represent a third state indicating that the
    legal hold status cannot be discerned due to insufficient permissions.
    For this reason, the type of the legal hold field has been changed.
    
    Change-Id: I96550386fa722635492899d169ad4ddfdae36a26

--- go.mod
+++ go.mod
@@ -30,6 +30,7 @@ require (
 	golang.org/x/crypto v0.19.0
 	golang.org/x/sync v0.6.0
 	golang.org/x/sys v0.20.0
+	google.golang.org/protobuf v1.32.0
 	gopkg.in/yaml.v2 v2.4.0
 	storj.io/drpc v0.0.35-0.20240709171858-0075ac871661
 	storj.io/eventkit v0.0.0-20240415002644-1d9596fee086
@@ -108,7 +109,6 @@ require (
 	google.golang.org/genproto/googleapis/api v0.0.0-20240205150955-31a09d347014 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240304161311-37d4d3c04a78 // indirect
 	google.golang.org/grpc v1.62.0 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pb/metainfo.pb.go
+++ pb/metainfo.pb.go
@@ -7,6 +7,7 @@ import (
 	time "time"
 
 	proto "github.com/gogo/protobuf/proto"
+	wrapperspb "google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -1343,8 +1344,8 @@ type Object struct {
 	EncryptionParameters *EncryptionParameters `protobuf:"bytes,13,opt,name=encryption_parameters,json=encryptionParameters,proto3" json:"encryption_parameters,omitempty"`
 	// retention is omitted if the object has no retention period or the request lacks the permission to read it.
 	Retention *Retention `protobuf:"bytes,20,opt,name=retention,proto3" json:"retention,omitempty"`
-	// legal_hold is omitted if the object has no legal hold or the request lacks the permission to read it.
-	LegalHold bool `protobuf:"varint,21,opt,name=legal_hold,json=legalHold,proto3" json:"legal_hold,omitempty"`
+	// legal_hold is omitted if the request lacks the permission to read the object's legal hold status.
+	LegalHold *wrapperspb.BoolValue `protobuf:"bytes,21,opt,name=legal_hold,json=legalHold,proto3" json:"legal_hold,omitempty"`
 	// total_size of object.
 	TotalSize int64 `protobuf:"varint,14,opt,name=total_size,json=totalSize,proto3" json:"total_size,omitempty"`
 	// size of inline part of object.
@@ -1478,11 +1479,11 @@ func (m *Object) GetRetention() *Retention {
 	return nil
 }
 
-func (m *Object) GetLegalHold() bool {
+func (m *Object) GetLegalHold() *wrapperspb.BoolValue {
 	if m != nil {
 		return m.LegalHold
 	}
-	return false
+	return nil
 }
 
 func (m *Object) GetTotalSize() int64 {
